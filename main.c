150604224aa1e4ae695ef4e0b18dad17dd1a5cbd
Steve Choo <pliable@gmail.com>
2020-03-25 01:57:01 -0700

    progress or something

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+
+#define SUCCESS 1
+#define FAILURE 0
+
 
 /**
    catty - concatenate files
@@ -15,12 +20,35 @@
 
    @author Steve Choo
 */
-int main(int argc, char *argv[]) {
+
+//maybe return int to denote success or failure?
+int print_file(FILE* fileToRead);
+int main(int argc, char* argv[]) {
+   FILE* fp;
+   int i;
    //quick and dirty check for proper amount of args
+   /*
    if(argc != 2) {
       fprintf(stderr, "Check yo arguments, son. Usage: cat <file>\n");
       exit(EXIT_FAILURE);
    }
+   */
+
+   //starting at 1 since arg0 is program itself
+   for(i = 1; i < argc; i++) {
+      fp = fopen(argv[i], "r");
+      if(fp == NULL) {
+         perror("Couldn't open file for some fucked up reason. Sucks");
+         continue;
+      }
+      print_file(fp);
+   }
 
    return 0;
+}
+
+int print_file(FILE* fileToRead) {
+   printf("oh whee for now");
+
+   return SUCCESS;
 }
